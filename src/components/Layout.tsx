b49b66c3e259dbcfcd5ca14e94691f74f337f8a4
José Roberto Pereira Dos Santos <rob_berty@hotmail.com>
2023-10-04 11:01:45 -0300

    Criando o Layout da aplicação

---
+++ src/components/Layout.tsx
@@ -0,0 +1,24 @@
+import Titulo from "./Titulo";
+
+/* Criando um componente Filho, essa é uma forma de garantir que algumas propriedades sejam
+exigidas no component
+*/
+interface LayoutProps {
+    titulo: string
+    children: any
+}
+
+// Criando o Layout do projeto
+export default function Layout(props: LayoutProps) {
+    return (
+        <div className={`
+            flex flex-col w-2/3
+            bg-white text-gray-800
+        `}>
+            <Titulo>{props.titulo}</Titulo>
+            <div>
+
+            </div>
+        </div>
+    )
+}
