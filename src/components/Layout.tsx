672a4c270d26ef8a8cab79d11caeea6aaf89dabd
José Roberto Pereira Dos Santos <rob_berty@hotmail.com>
2023-10-04 13:15:37 -0300

    Configuração do Layout ok

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,11 +13,11 @@ export default function Layout(props: LayoutProps) {
     return (
         <div className={`
             flex flex-col w-2/3
-            bg-white text-gray-800
+            bg-white text-gray-800 rounded-md
         `}>
             <Titulo>{props.titulo}</Titulo>
-            <div>
-
+            <div className="p-6">
+                {props.children}
             </div>
         </div>
     )
--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,10 +1,13 @@
 export default function Titulo(props) {
     return (
         <div className="flex flex-col justify-center">
-            <h1 className="px-5 py-2">
+            <h1 className="px-5 py-2 text-2xl">
                 {props.children}
             </h1>
-            <hr />
+            <hr className="border-2 border-purple-500" />
         </div>
     )
 }
+/*
+ * Função do Titulo da aplicação 
+*/
